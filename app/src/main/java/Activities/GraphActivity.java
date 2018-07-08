8499858f83428e6f8e19a843afc56fb0e6a3e3fd
Siddharth Bisht <siddharthBisht31@gmail.com>
2018-07-08 20:46:49 +0530

    The graph activity added to display the graphs based on user data

---
+++ app/src/main/java/Activities/GraphActivity.java
@@ -0,0 +1,15 @@
+package Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import siddharthbisht.targettracker.R;
+
+public class GraphActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+    }
+}
